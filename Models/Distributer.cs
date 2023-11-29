b7155033d6dde7dbafe696842472f84d8d37fe82
YuhanPizza <107896556+YuhanPizza@users.noreply.github.com>
2023-11-29 12:40:51 -0500

    Initialized Models

---
+++ Models/Distributer.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieReviewApp.Models
+{
+	public class Distributer
+	{
+        public int Id { get; set; }
+		public string Company { get; set; }
+		public string Address { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieReviewApp.Models
+{
+	//plain old CLR objects / Models
+	//used to represent database
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime ReleaseDate { get; set; }
+	}
+}
