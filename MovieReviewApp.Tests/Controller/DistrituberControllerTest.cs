ec3ac35a18b72a3952cb5a4856f736b092e8a9fe
YuhanPizza <107896556+YuhanPizza@users.noreply.github.com>
2024-01-07 17:13:28 -0500

    Finished all DistributerController Unit Tests Results: All Tests Passed!

--- MovieReviewApp.Tests/Controller/DistrituberControllerTest.cs
+++ MovieReviewApp.Tests/Controller/DistrituberControllerTest.cs
@@ -68,5 +68,85 @@ namespace MovieReviewApp.Tests.Controller
 			result.Should().NotBeNull();
 			result.Should().BeOfType(typeof(OkObjectResult));
 		}
+
+		[Fact]
+		public void DistributerController_GetMovieByDistributerId_ReturnOk()
+		{
+			//Arrange
+			int distributerId = 1;
+			var movieDto = A.Fake<List<MovieDto>>();
+			A.CallTo(() => _distributerRepository.DistributerExists(distributerId)).Returns(true);
+			A.CallTo(() => _mapper.Map<List<MovieDto>>(_distributerRepository.GetMovieByDistributer(distributerId))).Returns(movieDto);
+			var controller = new DistributerController(_distributerRepository, _countryRepository, _mapper);
+
+			//Act
+			var result = controller.GetMovieByDistributerId(distributerId);
+
+			//Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+		}
+
+		[Fact]
+		public void DistributerController_CreateDistributer_ReturnOk()
+		{
+			//Arrange
+			int countryId = 1;
+			var distributerCreate = A.Fake<DistributerDto>();
+			var distributer = A.Fake<Distributer>();
+			var country = A.Fake<Country>();
+			A.CallTo(() => _distributerRepository.GetDistributersTrimToUpper(distributerCreate)).Returns(null);
+			A.CallTo(() => _mapper.Map<Distributer>(distributerCreate)).Returns(distributer);
+			A.CallTo(() => _countryRepository.GetCountry(countryId)).Returns(country);
+			A.CallTo(() => _distributerRepository.CreateDistributer(distributer)).Returns(true);
+			var controller = new DistributerController(_distributerRepository, _countryRepository, _mapper);
+
+			//Act
+			var result = controller.CreateDistributer(countryId, distributerCreate);
+
+			//Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+		}
+
+		[Fact]
+		public void DistributerController_UpdateDistributer_ReturnOk()
+		{
+			//arrange
+			int distributerId = 1;
+			var distributerUpdate = A.Fake<DistributerDto>();
+			var distributer = A.Fake<Distributer>();
+			distributerUpdate.Id = distributerId; //so they match 
+			A.CallTo(() => _distributerRepository.DistributerExists(distributerId)).Returns(true);
+			A.CallTo(() => _mapper.Map<Distributer>(distributerUpdate)).Returns(distributer);
+			A.CallTo(() => _distributerRepository.UpdateDistributer(distributer)).Returns(true);
+			var controller = new DistributerController(_distributerRepository, _countryRepository, _mapper);
+
+			//act
+			var result = controller.UpdateDistributer(distributerId, distributerUpdate);
+
+			//Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+		}
+
+		[Fact]
+		public void DistributerController_DeleteDistributer_ReturnOk()
+		{
+			//Arrange
+			int distributerId = 1;
+			var distributerDelete = A.Fake<Distributer>();
+			A.CallTo(() => _distributerRepository.DistributerExists(distributerId)).Returns(true);
+			A.CallTo(() => _distributerRepository.GetDistributer(distributerId)).Returns(distributerDelete);
+			A.CallTo(() => _distributerRepository.DeleteDistributer(distributerDelete)).Returns(true);
+			var controller = new DistributerController(_distributerRepository, _countryRepository, _mapper);
+
+			//Act
+			var result = controller.DeleteDistributer(distributerId);
+
+			//Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+		}
     }
 }
