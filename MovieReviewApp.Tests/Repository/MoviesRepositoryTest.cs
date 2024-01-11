903aa400db279e6e4e48a93003bbf7f3039cbbe7
YuhanPizza <107896556+YuhanPizza@users.noreply.github.com>
2024-01-10 21:11:31 -0500

    MoviesRepository, Tested UpdateMovie : Passed! (just for fun unecessary)

--- MovieReviewApp.Tests/Repository/MoviesRepositoryTest.cs
+++ MovieReviewApp.Tests/Repository/MoviesRepositoryTest.cs
@@ -141,5 +141,53 @@ namespace MovieReviewApp.Tests.Repository
 			result.Should().NotBe(false);
 			result.Should().Be(true);
 		}
+
+		[Fact]
+		public async void MoviesRepository_UpdateMovie_ReturnTrue()
+		{
+			//arrange
+			int distributerId = 1;
+			int categoryId = 1;
+			var movieUpdate = new Movie()
+			{
+				Title = "Avengers: Endgame",
+				Description = "AVENGERS: ENDGAME is set after Thanos' catastrophic use of the Infinity Stones randomly wiped out half of Earth's population in Avengers: Infinity War. Those left behind are desperate to do something -- anything -- to bring back their lost loved ones.",
+				ReleaseDate = new DateTime(2019, 4, 26),
+				MovieCategories = new List<MovieCategory>()
+							{
+								new MovieCategory { Category = new Category() { Name = "Action"}},
+								new MovieCategory { Category = new Category() { Name = "Science Fiction"}},
+								new MovieCategory { Category = new Category() { Name = "Drama"}},
+								new MovieCategory { Category = new Category() { Name = "Fantasy"}}
+							},
+				Reviews = new List<Review>()
+							{
+								new Review { Title="Perfect",Text = "Phenomenally entertaining and a masterpiece!!! The Russo brothers nailed it again!!! I'm so grateful they're directing these movies. It makes them so much better and everything is so well thought out. ", Rating = 5,
+								Reviewer = new Reviewer(){ FirstName = "Sheikh", LastName = "Akbar" } },
+								new Review { Title="Avengers Fanboy",Text = "Thank you, MCU, Kevin Feige and all the directors for giving us such a great time (mostly with all the movies). Thank you Russo Brothers ( The \"Real Heroes\") in directing the Civil War, Infinity War and finally giving their best (appropriately) in Endgame and putting up or even surpassing the expectations. It's the best time I had in a Cinema Hall after a  long time! ", Rating = 5,
+								Reviewer = new Reviewer(){ FirstName = "Bhaskar", LastName = "Khurana" } },
+								new Review { Title="Epic!",Text = "Where to begin, where to begin! You know a movie is outstanding when the end credits alone are more epic than the majority of films released in the last 20 years! This film is the pure definition of an emotional roller coaster and throughout its run time brings about fascination, humor, sadness, incredible excitement, and sheer finality.", Rating = 5,
+								Reviewer = new Reviewer(){ FirstName = "Lorenzo", LastName = "Roman" } },
+							},
+				Distributer = new Distributer()
+				{
+					Company = "Walt Disney Studios Motion Pictures",
+					Address = "500 South Buena Vista Street, Burbank",
+					Country = new Country()
+					{
+						Name = "USA"
+					}
+				}
+			};
+			var dbContext = await GetDatabaseContext();
+			var moviesRepository = new MoviesRepository(dbContext);
+
+			//act
+			var result = moviesRepository.UpdateMovie(distributerId, categoryId, movieUpdate);
+
+			//Assert
+			result.Should().NotBe(false);
+			result.Should().Be(true);
+		}
     }
 }
